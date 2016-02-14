fe08cae09e60293dc65d32fc3c0f52c5e610a6cd
Chars García <chars24@gmail.com>
2016-02-14 13:21:41 -0600

    Cambiando las urls para que sean en ingles

--- products/urls.py
+++ products/urls.py
@@ -6,9 +6,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^$', views.ProductList.as_view(), name='hello'),
     url(r'^product/(?P<pk>[0-9]+)/$', views.ProductDetail.as_view(), name='detail'),
-	url(r'^nuevo$', views.ProductNew.as_view(), name='new'),
-	url(r'^editar/(?P<pk>\d+)$', views.ProductEdit.as_view(), name='edit'),
-	url(r'^borrar/(?P<pk>\d+)$', views.ProductDelete.as_view(), name='delete'),
+	url(r'^new$', views.ProductNew.as_view(), name='new'),
+	url(r'^edit/(?P<pk>\d+)$', views.ProductEdit.as_view(), name='edit'),
+	url(r'^delete/(?P<pk>\d+)$', views.ProductDelete.as_view(), name='delete'),
 
 	url(r'^login$', views.auth_login, name='authentication'),
 	url(r'^logout$', auth_views.logout, {'next_page': '/'}, name='logout'),
